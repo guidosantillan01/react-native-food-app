2443bfd8800cb67db93fd11f3875850626c7aa66
Guido Santillan <guidosantillan01@gmail.com>
2019-12-19 20:03:45 +0100

    Empty elements

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -16,7 +16,7 @@ const SearchScreen = () => {
   };
 
   return (
-    <View style={{ flex: 1 }}>
+    <>
       <SearchBar
         term={term}
         onTermChange={setTerm}
@@ -36,7 +36,7 @@ const SearchScreen = () => {
           title="Big Spender"
         />
       </ScrollView>
-    </View>
+    </>
   );
 };
 
