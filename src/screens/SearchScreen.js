d6a3b3ae5da89ed085b6ed6cc1d1307e6226640b
Guido Santillan <guidosantillan01@gmail.com>
2019-12-19 10:35:39 +0100

    Setup and configuration of SearchScreen

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,12 +2,18 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 import SearchBar from '../components/SearchBar';
+import ResultsList from '../components/ResultsList';
 
 import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
+
+  const filterResultsByPrice = price => {
+    // price === '$' || '$$' || '$$$'
+    return results.filter(result => result.price === price);
+  };
 
   return (
     <View>
@@ -17,8 +23,11 @@ const SearchScreen = () => {
         onTermSubmit={() => searchApi(term)}
       />
       <Text>Search Screen</Text>
-      <Text>We have found {results.length} results</Text>
       {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender" />
     </View>
   );
 };
